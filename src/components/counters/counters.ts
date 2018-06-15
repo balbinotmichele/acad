43227575ffb370b6d4f5a339e642097c94b23184
Michele Balbinot <michele.balbinot@marconirovereto.it>
2018-06-15 17:07:27 +0200

    counters now bound to database

--- src/components/counters/counters.ts
+++ src/components/counters/counters.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServiceDbAcadProvider } from '../../providers/service-db-acad/service-db-acad';
 
 /**
  * Generated class for the CountersComponent component.
@@ -11,12 +12,32 @@ import { Component } from '@angular/core';
   templateUrl: 'counters.html'
 })
 export class CountersComponent {
+  email : string;
+  hours : number;
+  tests : number;
+  experiments : number;
+  sessions : number;
 
-  text: string;
+  constructor(private sd : ServiceDbAcadProvider) {
+    this.email = sessionStorage.getItem("UserEmail") || "";
+    this.sd.GetOreEsperimenti(this.email)
+      .subscribe(res => {
+        this.hours = res[0].OreEsperimenti;
+      });
 
-  constructor() {
-    console.log('Hello CountersComponent Component');
-    this.text = 'Hello World';
+    this.sd.GetNumeroTest(this.email)
+      .subscribe(res => {
+        this.tests = res[0].NumeroTest;
+      });
+
+    this.sd.GetNumeroEsperimenti(this.email)
+      .subscribe(res => {
+        this.experiments = res[0].NumeroEsperimenti;
+      });
+
+    this.sd.GetNumeroSessioni(this.email)
+      .subscribe(res => {
+        this.sessions = res[0].NumeroSessioni;
+      });
   }
-
 }
