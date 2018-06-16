a7adbcb38db52ed8646a438db491c8913e85735f
Michele Balbinot <michele.balbinot@marconirovereto.it>
2018-06-16 20:05:30 +0200

    added delete button

--- src/components/experiments-home/experiments-home.ts
+++ src/components/experiments-home/experiments-home.ts
@@ -1,5 +1,5 @@
 import { ServiceDbAcadProvider } from './../../providers/service-db-acad/service-db-acad';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Esperimento } from '../../types/types';
 import { NavController } from 'ionic-angular';
 
@@ -7,7 +7,7 @@ import { NavController } from 'ionic-angular';
   selector: 'experiments-home',
   templateUrl: 'experiments-home.html'
 })
-export class ExperimentsHomeComponent {
+export class ExperimentsHomeComponent implements OnInit {
   @Output() experimentClick : EventEmitter<Esperimento> = new EventEmitter();
 
   experiments: Esperimento[];
@@ -15,8 +15,10 @@ export class ExperimentsHomeComponent {
 
   errmsg : string;
 
-  constructor(private sd : ServiceDbAcadProvider, public navCtrl : NavController) {
-    this.email = sessionStorage.getItem('UserEmail');
+  constructor(private sd : ServiceDbAcadProvider, public navCtrl : NavController) {}
+
+  ngOnInit() {
+    this.email = JSON.parse(sessionStorage.getItem('User')).Email;
     this.sd.GetEsperimenti(this.email)
       .subscribe(res => {
         this.experiments = res
@@ -30,4 +32,23 @@ export class ExperimentsHomeComponent {
     this.experimentClick.emit(newExp);
   }
 
+  // CopyExperiment(exp : Esperimento) {
+  //   exp.CodEsperimento = 0;
+  //   exp.NomeEsperimento += " - Copia";
+  //   this.sd.EditAddEsperimento(exp)
+  //     .subscribe(res => {
+  //       exp = res[0];
+  //       this.experiments.push(exp);
+  //     })
+  // }
+
+  DeleteExperiment(exp : Esperimento, i : number) {
+    this.sd.DeleteEsperimento(exp.CodEsperimento)
+      .subscribe(res => {
+        this.ngOnInit();
+        // this.experiments.splice(i, 1);
+      },
+      errorCode => this.errmsg = errorCode
+    )
+  }
 }
